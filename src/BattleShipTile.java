18010c2513e333f607ea414254314bd3f49df95c
ZGrahamz <graham_zach@outlook.com>
2024-11-11 23:57:28 -0500

    Initial commit

---
+++ src/BattleShipTile.java
@@ -0,0 +1,18 @@
+public class BattleShipTile {
+    private int row;
+    private int col;
+
+    public BattleShipTile(int row, int col) {
+        super();
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
